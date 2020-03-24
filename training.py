864b6aa5a1e8fd82018e89130f2ef001ecc6d3de
DesHis <55975611+DesHis@users.noreply.github.com>
2020-03-24 15:09:41 +0200

    epic :DDD

--- training.py
+++ training.py
@@ -4,5 +4,5 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 model_trainer = ModelTraining()
 model_trainer.setModelTypeAsResNet()
-model_trainer.setDataDirectory(r"C:\Users\Altti\Desktop\pöhinä")
+model_trainer.setDataDirectory(r"C:\Users\Altti\Desktop\tekoäly")
 model_trainer.trainModel(num_objects=2, num_experiments=200, enhance_data=True, batch_size=16, show_network_summary=True)
