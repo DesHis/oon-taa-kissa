ab7614d10c0fd3bc1125a781646fa616d219e515
DesHis <55975611+DesHis@users.noreply.github.com>
2020-03-24 14:49:30 +0200

    Update tunnistus.py

--- tunnistus.py
+++ tunnistus.py
@@ -1,19 +1,32 @@
 from imageai.Prediction.Custom import CustomImagePrediction
 import os
 
+print('Kuvan täytyy olla samassa kansiossa kuin tämän python tiedoston.')
+print('Syötä kuvan nimi (esim kuva.jpg): ')
+
 execution_path = os.getcwd()
+kuva = input()
+dir = os.path.dirname(os.path.realpath(__file__))
+dir.replace("\\","\\\\")
+
+print("")
+print("Analysoidaan kuvaa...")
+print("")
 
 prediction = CustomImagePrediction()
 prediction.setModelTypeAsResNet()
-prediction.setModelPath("C:\\Users\\Altti\\Desktop\\model_ex-085_acc-0.966261.h5")
-prediction.setJsonPath("C:\\Users\\Altti\\Desktop\\model_class.json")
+prediction.setModelPath(dir+"\\model_ex-085_acc-0.966261.h5")
+prediction.setJsonPath(dir+"\\model_class.json")
 prediction.loadModel(num_objects=2)
 
-predictions, probabilities = prediction.predictImage("C:\\Users\\Altti\\Desktop\\testi12.png", result_count=2)
+
+predictions, probabilities = prediction.predictImage(dir+"\\"+kuva, result_count=2)
 
 p1, p2 = zip(predictions, probabilities)
 
 varmuus=str(p1[1])[:4]
+
+print("")
 
 if p1[0]=="kissa":
     print("oon tää kissa :D ("+varmuus+"% varmuus)")
